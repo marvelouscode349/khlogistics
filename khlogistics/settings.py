887569e1c67df3528f73327686e2613a3b72fcf2
marvelouscode349 <marvelouspeter90@gmail.com>
2023-09-20 12:04:27 +0100

    static files aws

--- khlogistics/settings.py
+++ khlogistics/settings.py
@@ -181,3 +181,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL =  None
 AWS_S3_VERITY = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
